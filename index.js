8972d6b0f27eab165c0e41d166159c175627b281
Rhino <andre.bazoli@watchguard.com>
2018-02-27 18:43:02 -0300

    Handling cards match behavior

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 import { shuffle } from './scripts/shuffle';
+import { isCardsMatch } from './scripts/checkClick';
+
 const cards = [].concat(...document.querySelectorAll('.card'));
 const ul = document.querySelector('ul');
+
+let cardsSelected = [];
+cards.forEach((card) => {
+  card.addEventListener('click', event => {
+      // TODO: Add a moviment
+      cardsSelected.push(card);
+      if (cardsSelected.length === 2) {
+        if (isCardsMatch(...cardsSelected)) {
+          // TODO: Show 'match' animation
+          console.log('Cards match');
+          cardsSelected = [];
+        } else {
+          // TODO: Remove a point
+          // TODO: Show 'not match' animation
+          console.log('Cards do not match');
+          cardsSelected = [];
+        }
+      }
+    }, true)
+});
 
 shuffle(cards)
   .forEach(card => {
     ul.appendChild(card);
   });
+
---
+++ scripts/isCardsMatch.js
@@ -0,0 +1,3 @@
+export function isCardsMatch(card1, card2) {
+    return card1.classList[1] === card2.classList[1];
+}
