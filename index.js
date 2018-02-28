587d773a30e422441a4fb7b9bc9248603b9b7a29
Rhino André <rhinoandre@gmail.com>
2018-02-27 21:40:44 -0300

    Adding rotation to cards when selecting

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import { shuffle } from './scripts/shuffle';
-import { isCardsMatch } from './scripts/checkClick';
+import { isCardsMatch } from './scripts/isCardsMatch';
 
 const cards = [].concat(...document.querySelectorAll('.card'));
 const ul = document.querySelector('ul');
-
 let cardsSelected = [];
+
+function selectCard(card) {
+  if (!cardsSelected.some(cardSelected => cardSelected === card)) {
+    card.classList.remove('select');
+    cardsSelected.push(card);
+  }
+}
+
+function hideCard(){
+  cardsSelected.forEach(card => card.classList.add('select'));
+  cardsSelected = [];
+}
+
 cards.forEach((card) => {
   card.addEventListener('click', event => {
       // TODO: Add a moviment
-      cardsSelected.push(card);
+      selectCard(card);
       if (cardsSelected.length === 2) {
         if (isCardsMatch(...cardsSelected)) {
           // TODO: Show 'match' animation
@@ -18,7 +30,7 @@ cards.forEach((card) => {
           // TODO: Remove a point
           // TODO: Show 'not match' animation
           console.log('Cards do not match');
-          cardsSelected = [];
+          setTimeout(hideCard, 3000);
         }
       }
     }, true)
